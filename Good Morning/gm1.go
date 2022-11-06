888b6f1a8113a573c962ba9bb9da2d562e826348
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 11:15:33 +0530

    Create gm1.go

---
+++ Good Morning/gm1.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+func main() {
+    
+// good morning
+     
+fmt.Println("🌷🌹🍃🌹🌷G")
+fmt.Println("┏╮╭┓┏╮╭┓ O")
+fmt.Println("╰╭╮╯╰╭╮╯ O")
+fmt.Println("╭╰╯╮╭╰╯╮ D")
+fmt.Println("┗╯╰┛┗╯╰┛ ")
+fmt.Println("🌷🌹🍃🌹🌷 M")
+fmt.Println("┏╮╭┓┏╮╭┓  O")
+fmt.Println("╰╭╮╯╰╭╮╯  R")
+fmt.Println("╭╰╯╮╭╰╯╮  N")
+fmt.Println("┗╯╰┛┗╯╰┛   I")
+fmt.Println("🌷🌹🍃🌹🌷 N")
+fmt.Println("┏╮╭┓┏╮╭┓  G")
+fmt.Println("╰╭╮╯╰╭╮╯  ")
+fmt.Println("╭╰╯╮╭╰╯╮  D")
+fmt.Println("┗╯╰┛┗╯╰┛  E")
+fmt.Println("🌷🌹🍃🌹🌷 A")
+fmt.Println("      FRIEND     ")
+}
